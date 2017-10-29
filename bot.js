b03cefd644ed9c5cf660be5e7c761e86dc289bac
August <view92gaming@gmail.com>
2017-10-29 06:09:43 +0000

    Add new file

---
+++ bot.js
@@ -0,0 +1,26 @@
+const YumiBoat = require('./YumiBoat');
+YumiBoat.config = require('./lib/config.json');
+const Yumi = new YumiBoat(YumiBoat.config.api_keys.Discord.login);
+const games = require('./lib/games.json');
+YumiBoat.Log = console.log;
+
+Yumi.bot.on('ready', () =>
+{
+    Yumi.bot.editStatus(null,
+    {
+       name: `yumibot.party | !y.help`,
+       type: 0
+    });
+    Yumi.bot.serverCount();
+    YumiBoat.Log.info(`[BOT]: YumiBoat is online.`);
+});
+
+Yumi.bot.on('messageCreate', (msg) =>
+{
+   if (!msg.content.startsWith(YumiBoat.config.prefix) || msg.author.bot) return;
+
+   const command = msg.content.split(" ").shift().slice(YumiBoat.config.prefix.length);
+   Yumi.handle(msg, command);
+});
+
+Yumi.start();
